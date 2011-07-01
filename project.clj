45ca59d4d45b6e21f8f01c55c2603b1090d7a50c
Josh Daghlian <daghlian@gmail.com>
2011-07-01 11:14:15 -0400

    Correct: Problem 44

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject anothereuler "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]
+		 [org.clojure/clojure-contrib "1.2.0"]]
+  :jvm-opts ["-Xmx2g -XX:+UseCompressedOops"])
---
+++ test/anothereuler/test/core.clj
@@ -0,0 +1,6 @@
+(ns anothereuler.test.core
+  (:use [anothereuler.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
